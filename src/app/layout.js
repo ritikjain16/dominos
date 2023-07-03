f322151f53016f14125d60631565e5f1b0948240
Ritik Jain <ritik9628@gmail.com>
2023-07-03 15:59:07 +0530

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,40 +2,35 @@ import MenuProvider from "@/context/menu/menuContext";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Dominos",
-  description:
-    "Domino's is OPEN to serve you. Order all your favorite pizzas and we will ensure Zero contact delivery for all order.",
-  icons: {
-    icon: [
-      { url: "/Dominos/dominos.png" },
-      new URL("/Dominos/dominos.png", "https://example.com"),
-    ],
+// export const metadata = {
 
-    shortcut: "/Dominos/dominos.png",
-    apple: "/Dominos/dominos.png",
-    other: {
-      rel: "apple-touch-icon-precomposed",
-      url: "/Dominos/dominos.png",
-    },
-    themeColor: '#005491',
-  },
-};
+// };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <Head>
+      <head>
+        <meta charset="utf-8" />
+        <link rel="icon" href="/Dominos/dominos.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#005491" />
         <meta name="msapplication-navbutton-color" content="#005491" />
-        <link rel="icon" href="/Dominos/dominos.png" />
-      </Head> */}
+        <meta
+          name="description"
+          content="Domino's is OPEN to serve you. Order all your favorite pizzas and we will ensure Zero contact delivery for all order."
+        />
+        <link rel="apple-touch-icon" href="/Dominos/dominos.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <title>Dominos</title>
+      </head>
       <body className={inter.className}>
         <MenuProvider>{children}</MenuProvider>
       </body>
+      <Script src="/myscript.js" />
     </html>
   );
 }
